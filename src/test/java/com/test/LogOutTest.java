c54e239970163a05e363539cbab38f568b05a624
satya <satyanarayana.reddy@kaplan.com>
2020-04-18 09:06:34 -0400

    Added logout File

---
+++ src/test/java/com/test/LogOutTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LogOutTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.co.in");
+	}
+
+}
